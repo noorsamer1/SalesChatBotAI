f0fa3f99e5faaf1cc12b2fe72ab7ad020fc65ad2
noorsamer1 <nooraldeen529@gmail.com>
2025-07-16 17:00:47 +0300

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Allow frontend (Vite on port 5174) to access backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174"],
+    allow_origins=["http://localhost:5174","http://localhost:5173","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
